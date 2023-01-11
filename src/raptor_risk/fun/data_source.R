fd935cb654fb2540ffd7104fdd9bcd02fe30985d
kgaythorpe <k.gaythorpe@imperial.ac.uk>
2023-01-11 10:34:57 +0000

    first fun

---
+++ src/raptor_risk/fun/data_source.R
@@ -0,0 +1,14 @@
+load("data/shp/shapes.RData")
+
+shp_file@data$NAME_2[shp_file@data$NAME_2=="Bujenje (?)"] <- "Buliisa"
+
+list_of_countries <-  unique(shp_file$NAME_0 ) 
+list_of_ISOs <-  unique(shp_file$ISO) 
+
+radiation = rbind(readRDS("data/rad1.rds"), readRDS("data/rad2.rds"), readRDS("data/rad3.rds"), 
+                  readRDS("data/rad4.rds"), readRDS("data/rad5.rds"), readRDS("data/rad6.rds"))
+ #----------------------------------------------
+age_prop <- read.csv("data/age_prop.csv", stringsAsFactors = FALSE)
+
+#----------------------------------------------
+yf_coverage <- readRDS("data/YF_vac_cov.RDS")
