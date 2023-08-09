14fac29a6888f46d859746ae7a19d96eacfe7787
kgaythorpe <k.gaythorpe@imperial.ac.uk>
2023-08-09 14:47:08 +0100

    updating to new foi estimates

--- src/raptor_risk_per_country/script.R
+++ src/raptor_risk_per_country/script.R
@@ -8,14 +8,14 @@ in_template <- make_template(c("Ethiopia", "Sudan",
 
 in_template <- in_template %>% mutate(region_id = gsub("_$", "", gsub('.{2}$', '', District_id)))
 
-foi <- read.csv("data/FOI_R0_med_values_af_yem.csv", stringsAsFactors = FALSE)
-foi <- foi %>% mutate(region_id = gsub("\\.", "_", gsub('.{2}$', '', regions)))
+foi <- read.csv("data/FOI_R0_med_values_af_yem_new.csv", stringsAsFactors = FALSE)
+foi <- foi %>% mutate(region_id = gsub("\\.", "_", gsub('.{2}$', '', region)))
 
 # filter foi to template
 foi <- foi %>% filter(region_id %in% in_template$region_id)
 
 # join 'em up
-in_template <- in_template %>% left_join(foi %>% dplyr::select(region_id, FOI_med), by = "region_id")
-in_template <- in_template %>% mutate(FOI_med = ifelse(is.na(FOI_med), 0, FOI_med))
+in_template <- in_template %>% left_join(foi %>% dplyr::select(region_id, FOI_50), by = "region_id")
+in_template <- in_template %>% mutate(FOI_med = ifelse(is.na(FOI_50), 0, FOI_50))
 
 fun_make_output_country_start(in_template, foi, ctry, size_input, n_sample)
